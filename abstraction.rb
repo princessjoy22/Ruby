27ad39679fad5d934cae4d3b04f4ee2df320c986
princessjoyibarretapopanes@gmail.com <princessjoyibarretapopanes@gmail.com>
2024-01-26 23:34:59 +0800

    updated abstraction

--- abstraction.rb
+++ abstraction.rb
@@ -1,17 +1,36 @@
-class AreaCalculator
-  def initialize(length, width)
-    @length = length
-    @width = width
+class SavingsAccount
+  attr_accessor :balance
+
+  def initialize(balance)
+    @balance = balance
+  end
+
+  public
+
+  def calculate_interest
+    puts "Calculating interest..."
+    interest = calculate_interest_amount
+    @balance += interest
+    interest
+  end
+
+  private
+
+  def calculate_interest_amount
+    puts "Calculating interest amount..."
+    @balance * interest_rate
+  end
+
+  protected
+
+  def interest_rate
+    0.01
   end
 end
 
-class rectangle < AreaCalculator
-  def compute
-    @length * @width
-end
-end
 
+savings_account = SavingsAccount.new(1000)
 
-
-rectangle = Rectangle.new(10,10)
-puts rectangle.compute
+puts "Savings balance before interest: #{savings_account.balance}"
+savings_account.calculate_interest
+puts "Savings balance after interest: #{savings_account.balance}"
