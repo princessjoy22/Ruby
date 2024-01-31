81c63b8897e9d044c4e2b6bb35fe03c52fdc3fe7
princessjoyibarretapopanes@gmail.com <princessjoyibarretapopanes@gmail.com>
2024-01-31 20:36:49 +0800

    activity isograms.rb added

---
+++ isograms.rb
@@ -0,0 +1,15 @@
+def is_isogram(word)
+    letters = Set.new
+    word.downcase.each_char do |letter|
+        if letter =~ /[a-z]/
+            return false if letters.include?(letter)
+        end
+        letters.add(letter)
+        end
+    true
+end
+
+puts is_isogram("Dermatoglyphics");
+puts is_isogram("aba");
+puts is_isogram("moOse");
+
