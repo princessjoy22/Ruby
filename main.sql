157b7328157b57ecca7142134d1af30f983df528
princessjoyibarretapopanes@gmail.com <princessjoyibarretapopanes@gmail.com>
2024-01-31 22:33:45 +0800

    update main.sql ,added JOIN activity

--- main.sql
+++ main.sql
@@ -1,7 +1,7 @@
 CREATE TABLE students (
-	id 			integer, 
+	id 			integer                  PRIMARY KEY, 
 	first_name 	character varying(100)   NOT NULL,
-	middle_name character varying(100)   NOT NULL,
+	middle_name character varying(100),
 	last_name 	character varying(100)   NOT NULL,
 	age        	integer 			     NOT NULL,
 	location 	character varying(100)   NOT NULL
@@ -29,11 +29,47 @@ DELETE FROM students where id = 6;
 SELECT COUNT(*)
 FROM students;
 
-SELECT first_name FROM students WHERE location = 'Manila';
+SELECT * FROM students WHERE location = 'Manila';
 
-SELECT AVG(age) FROM students;
+SELECT ROUND(AVG(age),2) FROM students;
 
 SELECT * from students ORDER BY age DESC;
 
+-- CREATE new TABLE  research_papers
 
+CREATE TYPE grades AS ENUM ('A', 'B', 'C', 'D', 'E', 'F', NULL);
 
+CREATE TABLE research_papers (
+    id              integer     NOT NULL,
+    student_id      integer,
+    grade           grades,
+    PRIMARY KEY (id),
+    FOREIGN KEY (student_id) REFERENCES students(id)
+);
+
+-- INSERT 10 RECORDS
+INSERT INTO research_papers (id, student_id, grade) VALUES
+(1, 1, 'A'),
+(2, 2, 'B'),
+(3, 3, 'C'),
+(4, 4, 'E'),
+(5, 5, 'D'),
+(6, 1, 'A'),
+(7, 2, 'A'),
+(8, 3, NULL),
+(9, 4, NULL),
+(10, 1, 'A');
+
+-- ALL STUDENTS WITH MULTIPLE RESEARCH PAPER
+SELECT students.first_name, students.last_name, COUNT(research_papers.id) AS number_of_research_papers
+FROM students
+INNER JOIN research_papers ON students.id = research_papers.student_id
+GROUP BY students.first_name, students.last_name
+HAVING COUNT(research_papers.id) > 1;
+
+-- ALL STUDENTS WITH UNGRADED RESEARCH PAPERS
+SELECT students.first_name, students.last_name, COUNT(research_papers.id) AS research_paper_id, COUNT(research_papers.grade) AS grade
+FROM students
+INNER JOIN research_papers ON students.id = research_papers.student_id
+GROUP BY students.first_name, students.last_name, research_papers.grade
+HAVING COUNT(research_papers.grade) = 0;
