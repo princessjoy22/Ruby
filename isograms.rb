9a33642920282388c2ff8a7b5def296e614145e7
princessjoyibarretapopanes@gmail.com <princessjoyibarretapopanes@gmail.com>
2024-01-31 20:58:28 +0800

    updated

--- isograms.rb
+++ isograms.rb
@@ -1,5 +1,10 @@
+# ISOGRAMS
+
+# Implement a function that determines whether a string that contains only letters is an isogram. Assume the empty string is an isogram. Ignore letter case.
+
 def is_isogram(word)
     letters = Set.new
+
     word.downcase.each_char do |letter|
         if letter =~ /[a-z]/
             return false if letters.include?(letter)
