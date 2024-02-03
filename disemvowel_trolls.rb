4df4a0a4ca4d64e1b82f0108a6a88708431984f3
princessjoyibarretapopanes@gmail.com <princessjoyibarretapopanes@gmail.com>
2024-02-03 09:33:17 +0800

    added disemvowel_trolls.rb activity

---
+++ disemvowel_trolls.rb
@@ -0,0 +1,16 @@
+# Write a method that takes a string and return a new string with all vowels removed.
+
+def disemvowel_trolls(comment)
+    return_message = []
+    vowels = ["a", "e", "i", "o", "u", "A", "E", "I", "O", "U"]
+
+    comment.each_char do |letter|
+        if vowels.include?(letter)
+        else
+        return_message.push(letter)
+        end
+    end
+    return_message.join
+end
+
+puts disemvowel_trolls("This website is for losers LOL!");
