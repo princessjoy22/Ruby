1a728579697f3ee322671b71d28b7e4dc1b9ab00
princessjoy22 <princessjoyibarretapopanes@gmail.com>
2024-03-26 20:22:34 +0800

    updated activity

--- reverse.rb
+++ reverse.rb
@@ -2,10 +2,10 @@ def reverse(str)
     reversed_str = ""
 
     str.each_char do |char|
-        reversed_str = char.to_s + reversed_str
+        reversed_str = char + reversed_str
     end
 
-    return reversed_str
+    reversed_str
 end
 
 puts reverse("tomato");
